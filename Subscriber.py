cd857d5a907e6c4c9ac80f5cafeb1e8ff62d2d44
Ailce <kohinamiko@sunmoon.ac.kr>
2023-01-13 21:12:08 +0900

    강의 4일차 영상 중간까지 확인 및 시뮬레이터 연습

--- Subscriber.py
+++ Subscriber.py
@@ -2,30 +2,72 @@
 
 # -*- coding:utf-8 -*-
 
-import rospy
-from std_msgs.msg import Int32
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-import numpy as np
-rospy.init_node('sub_test')
-cmd_vel = Twist()
+# import rospy
+# from std_msgs.msg import Int32
+# from geometry_msgs.msg import Twist
+# from turtlesim.msg import Pose
+# import numpy as np
+# rospy.init_node('sub_test')
+# cmd_vel = Twist()
 
-degree = np.pi/180
+# degree = np.pi/180
+
+# def data(msg):
+#     print("msg.x",msg.x)
+#     if 1<msg.x<10 and 1 < msg.y < 10:
+#         cmd_vel.linear.x = 1
+#         cmd_vel.angular.z = 0
+#     else:
+#         cmd_vel.angular.z = degree*90
+#     #print("msg.y",msg.y)
+#     #print("msg.theta",msg.theta)
+#     # cmd_vel.linear.x = msg.data / 10
+#     # cmd_vel.angular.z = msg.data % 10
+#     pub.publish(cmd_vel)
+#     pass
+
+# sub = rospy.Subscriber('/turtle1/pose',Pose, callback = data)
+# pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size= 1)
+# rospy.spin()
+
+####### simulator 사용 #######
+
+import rospy
+from std_msgs.msg import Float64
+from morai_msgs.msg import EgoVehicleStatus
+rospy.init_node("sub_test")
+
+rate = rospy.Rate(0.3)
+
+speed_accel = Float64()
+x = -2.26 # -2.26~2.26
+#speed_km = (1062.333479 * x) + 88.278849              # x가 커질수록 정확해짐
+speed_km = (1164.251442*x) -28.85558                   # x가 작을수록 정확해짐
+# 소수점 6자리를 구했으나 정확한 값이 아니라 오차가 있음
 
 def data(msg):
-    print("msg.x",msg.x)
-    if 1<msg.x<10 and 1 < msg.y < 10:
-        cmd_vel.linear.x = 1
-        cmd_vel.angular.z = 0
-    else:
-        cmd_vel.angular.z = degree*90
-    #print("msg.y",msg.y)
-    #print("msg.theta",msg.theta)
-    # cmd_vel.linear.x = msg.data / 10
-    # cmd_vel.angular.z = msg.data % 10
-    pub.publish(cmd_vel)
-    pass
+    #print("EgoVehicleStatus : ",msg)       
+    #print("car.velocity.x : ", msg.velocity.x) 
 
-sub = rospy.Subscriber('/turtle1/pose',Pose, callback = data)
-pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size= 1)
+    # if msg.velocity.x < 1:
+    #     pub.publish(speed_km)
+    #     #pub.publish(1170)
+
+    #     #speed += 10
+    # elif msg.velocity.x == 1:        
+    #     print("그만")
+    # elif msg.velocity.x > 1 :
+    #     print("허걱")
+    pub.publish(speed_km)
+    print("speed : {}, car.velocity.x : {} ".format(speed_km, msg.velocity.x))
+    pub_angle.publish(0.5)
+
+    #rate.sleep()
+
+    
+
+pub = rospy.Publisher("/commands/motor/speed",Float64,queue_size=3)
+pub_angle = rospy.Publisher("/commands/servo/position",Float64,queue_size=3)
+sub = rospy.Subscriber('/Ego_topic',EgoVehicleStatus,callback = data)
+
 rospy.spin()
---
+++ pub.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import Float64
+
+rospy.init_node("pub_motor_speed")
+
+pub_speed = rospy.Publisher("/commands/motor/speed",Float64,queue_size=3)
+pub_angle = rospy.Publisher("/commands/servo/position",Float64,queue_size=3)
+count = 100
+rate = rospy.Rate(2)
+
+while not rospy.is_shutdown():
+    count += 100
+    pub_speed.publish(count)
+    pub_angle.publish(0.5)
+    rate.sleep()
+
+
+'''
+각도는 0이 -19.5도고 1이 19.5도이다 입력값을 x 시뮬 각도가 y라 하자
+(0.-19.5) (1,19.5) 39 = y+19.5/x    0 <= x <= 1    -19.5 <= y <= 19.5
+y = 39x - 19.5
+
+speed = 1000일때 accel 대략 0.858225 a,b  x가 speed y가 car.velocity.x
+speed = 2000일때 accel 대략 1.799549 c,d
+x = y(1062.333479) + 88.278849 원하는 velocity.x 값을 y에 넣으면 됨 넣으면 됨
+
+speed : 2489.1525115399995 car.velocity.x : 2.16277  a,b
+speed : 1150.612328, car.velocity.x : 1.01307        c,d
+
+(2.16277,2489.152511) (1.01307,1150.612328)
+
+(2489.152511-1150.612328)/2.16277-1.01307 = y-2489.152511/x-2.16277
+
+(1338.539883)*(x-2.16277) = (y-2489.152511) * (1.1497)
+
+1164.251442*x -28.85558 = y
+
+1.1497*(x-c) = (y-d) * (1338.54018)
+0.0008589x - 0.988284 = y - 1.01307
+0.0008589x + 0.024786 = y
+
+
+
+'''
